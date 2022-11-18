7d3603d2780ac1f5d7c4c9aa3bf3665db7136869
Usuario <Usuario@LAPTOP-15CKS5M0>
2022-11-18 12:05:23 +0100

    creamos reina, después el metodo ejecutarOpcion que dependiendo de la opcionm se combierte en parametro, después creamos el metodo crearReinaDefecto que asignará al atributo de la clase reina, a continuación creamos el metodo crearReina color, después el metodo mover que mostrara un menú con las direcciones, después se hace el metodo mostarReina y ta por ultimo el metodo main

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,8 +1,72 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+
 public class MainApp {
-	
-	public static void main(String[] args) {
-		
-	}
+
+    private static Reina reina;
+
+    private static void ejecutarOpcion(int opcion) {
+        switch (opcion) {                     //sirve para dar ordenes 
+            case 1:
+                crearReinaDefecto();
+                mostrarReina();
+                break;
+            case 2:
+                crearReinaColor();
+                mostrarReina();
+                break;
+            case 3:
+                mover();
+                mostrarReina();
+            case 4:
+                Consola.despedirse();
+
+        default:
+        }
+        }
+
+
+    private static void crearReinaDefecto() {
+        reina = new Reina();
+
+    }
+    private static void crearReinaColor() {
+        reina = new Reina(Consola.elegirColor());
+
+    } 
+
+    private static void mover() { //metodo estatico 
+        try {
+            reina.mover(Consola.elegirDireccion(), Consola.elegirPasos());
+        } catch (OperationNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void mostrarReina() {
+        if (reina == null) {
+            throw new NullPointerException("No existe reina aún.");
+
+        }
+        System.out.println(reina);
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int opcion; 
+
+        do {
+            opcion = Consola.elegirOpcionMenu();
+            ejecutarOpcion(opcion);
+        } while (opcion != 4);
+
+
+
+
+    }
 }
